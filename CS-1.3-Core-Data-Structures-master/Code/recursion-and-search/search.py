79b1218306b405d29f869739a8c48b156a1166c7
SamirIngley <samir.ingle7@gmail.com>
2020-04-11 19:13:08 -0700

    search passes tests COMPLETED

--- CS-1.3-Core-Data-Structures-master/Code/recursion-and-search/search.py
+++ CS-1.3-Core-Data-Structures-master/Code/recursion-and-search/search.py
@@ -41,25 +41,91 @@ def binary_search_iterative(array, item):
     # TODO: implement binary search iteratively here
     # ASSUMING ARRAY IS SORTED
     # if the number exists it's between the first and last nums
-    found = False
-    new_array = array
-    while not found:
-        mid_index = floor(len(new_array/2))
-        print(mid_index)
-        index = 0
-        
-        if item[0] == array[mid_index][0]:
-            found = True
-            return mid_index 
+    # found = False
+    # new_array = array
+    # mid_index = int(floor(len(new_array)/2)-1)
 
-        elif item[0] > array[mid_index][0]:
-            new_array = array[mid_index:]
-        elif item[0] < array[mid_index][0]:
-            new_array = array[:mid_index]
+    # print(array)
+    # print(item, item[0])
+    # print(mid_index)
+    # print(array[mid_index])
+    # print(array[mid_index][0])
+
+    # while not found:
         
-        elif len(new_array) == 1:
+    #     mid_index = int(floor(len(new_array)/2)-1)
+    #     print('middex ', mid_index)
+
+
+    #     if item == new_array[mid_index]:
+    #         found = True
+    #         print('found: ', item, new_array[mid_index])
+    #         return mid_index 
+
+    #     elif item[0] > new_array[mid_index][0]:
+    #         new_array = new_array[mid_index:]
+    #         print('item: ', item, item[0])
+    #         print('array loc: ', new_array[mid_index])
+    #         print('item is greater so.. ', new_array)
+
+    #     elif item[0] < new_array[mid_index][0]:
+    #         new_array = new_array[:mid_index]
+    #         print('item: ', item, item[0])
+    #         print('array loc: ', new_array[mid_index])
+    #         print('item is less so.. ', new_array)
+        
+    #     elif len(new_array) == 1:
+    #         found = True
+    #         print('Not found: ', item, new_array[mid_index])
+    #         return None
+    
+
+    new_array = array
+    found = False
+    may_exist = True
+    letter = 0
+    index = None
+
+    while not found and may_exist:
+
+        middle = int(floor(len(new_array)/2 - 1))
+        print(middle, item, new_array[middle], item[0], new_array[middle][0], new_array)
+
+        if new_array[middle] == item:
+            print('found it', item, new_array[middle], new_array)
+            found = True
+            return array.index(item) 
+        
+        elif len(new_array) == 1 and new_array[0] != item:
+            print('out of items, no luck')
+            may_exist = False
             return None
+        
+        elif item[letter] == new_array[middle][letter]:
+            letter += 1
+            print('moved letter')
             
+        elif new_array[middle][letter] < item[letter]:
+            new_array = new_array[middle+1:]
+            letter = 0
+            print('item is greater')
+            print(new_array)
+        
+        elif new_array[middle][letter] > item[letter]:
+            new_array = new_array[:middle+1]
+            letter = 0
+            print('item is less')
+            print(new_array)
+
+
+
+
+
+
+
+
+
+
             
     # once implemented, change binary_search to call binary_search_iterative
     # to verify that your iterative implementation passes all tests
@@ -70,3 +136,5 @@ def binary_search_recursive(array, item, left=None, right=None):
     pass
     # once implemented, change binary_search to call binary_search_recursive
     # to verify that your recursive implementation passes all tests
+
+# binary_search([0], 0)
