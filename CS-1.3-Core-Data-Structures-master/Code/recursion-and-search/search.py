e4010a1144820df6a969a61f5710f60f1aa0237c
SamirIngley <samir.ingle7@gmail.com>
2020-04-12 18:18:10 -0700

    missing a return statement... program COMPLETE ready for submission

--- CS-1.3-Core-Data-Structures-master/Code/recursion-and-search/search.py
+++ CS-1.3-Core-Data-Structures-master/Code/recursion-and-search/search.py
@@ -85,48 +85,52 @@ def binary_search_recursive(array, item, left=None, right=None):
     # TODO: implement binary search recursively here
     letter = 0
 
-    if not left and not right:
+    if left== None and right==None: # if this is our first pas through
         left = 0
         right = len(array)-1
 
-    middle = left + int(ceil((right-left)/2)) # difference/2 + bottom = middle
-    
-    # print(left, int(ceil((right-left)/2)), 'right ', right, 'left ', left, 'subtract ', right-left, 'subtract/2 ', (right-left)/2)
-    print('between ', array[left], array [right])
+    # middle = left + int(ceil((right-left)/2)) # difference/2 + bottom = middle
+    middle = int((left+right)//2)
+    # print(left, int((right-left)//2), 'right ', right, 'left ', left, 'subtract ', right-left, 'subtract/2 ', (right-left)/2)
+    print(array)
+    print('between ', array[left], array[right])
+    print(item, left, right, middle, array[middle])
     if item == array[middle]:
         print('middle ', middle)
-        print('found: ', item, array[middle], left, right)
-        print(middle)
-        return middle
+        print('FOUND: ', item, array[middle], left, right)
+        print(int(middle))
+        # return array.index(item)
+        return int(middle) # bc we want the index
     elif right-left == 0:
+        print('right-left=0', right, left)
         print('dne')
         return None
-    elif right-left == 1:  # we do this because if we try to move a side over when only two items remain with our current strategy.. it breaks when we only have two items left.. we can't just go to one since we increment the left and right based on the middle. 
-        if item == array[left]:
-            print('hit me')
-            print(left)
-            return int(left)
-        else:
-            print('dne')
-            return None
+    # elif right-left == 1:  # we do this because if we try to move a side over when only two items remain with our current strategy.. it breaks when we only have two items left.. we can't just go to one since we increment the left and right based on the middle. 
+    #     if item == array[left]:
+    #         print('hit me')
+    #         print(left)
+    #         return int(left)
+    #     else:
+    #         print('dne')
+    #         return None
     
-    print(array[middle])
+    print('middle: ', array[middle])
     while item[letter] == array[middle][letter]:
         letter += 1
         print('letter up')
 
     if item[letter] > array[middle][letter]:
         print(item, item[letter], 'item is greater than ', array[middle], array[middle][letter])
-        left = middle
-        print('left = middle', middle)
+        left = middle+1
+        print('left = middle+1:', middle+1)
         print('right ', right)
-        binary_search_recursive(array, item, left, right)
+        return binary_search_recursive(array, item, left, right)
     elif item[letter] < array[middle][letter]:
         print(item, item[letter], 'item is less than ', array[middle], array[middle][letter])
-        right = middle
-        print('right = middle', middle)
+        right = middle-1
+        print('right = middle-1:', middle-1)
         print('left ', left)
-        binary_search_recursive(array, item, left, right) 
+        return binary_search_recursive(array, item, left, right) 
         
 
 
@@ -139,3 +143,7 @@ def binary_search_recursive(array, item, left=None, right=None):
 
 # binary_search(['alex', 'banana', 'cab', 'top', 'weird', 'zoo'], 'top')
 binary_search(['Alex', 'Brian', 'Julia', 'Kojin', 'Nabil', 'Nick', 'Winnie'], 'Alex')
+binary_search(['Alex', 'Brian', 'Julia', 'Kojin', 'Nabil', 'Nick', 'Winnie'], 'Brain')
+binary_search(['Alex', 'Brian', 'Julia', 'Kojin', 'Nabil', 'Nick', 'Winnie'], 'Julia')
+binary_search(['Alex', 'Brian', 'Julia', 'Kojin', 'Nabil', 'Nick', 'Winnie'], 'Winnie')
+binary_search(['Alex', 'Brian', 'Julia', 'Kojin', 'Nabil', 'Nick', 'Winnie'], 'Nick')
