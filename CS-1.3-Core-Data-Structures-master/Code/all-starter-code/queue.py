fa3273207715e1d26f9f900688bd99e375ab3690
SamirIngley <samir.ingle7@gmail.com>
2020-04-24 21:21:39 -0700

    arraystack and linkedliststack pass all tests and runtimes added

--- CS-1.3-Core-Data-Structures-master/Code/all-starter-code/queue.py
+++ CS-1.3-Core-Data-Structures-master/Code/all-starter-code/queue.py
@@ -76,26 +76,40 @@ class ArrayQueue(object):
     def is_empty(self):
         """Return True if this queue is empty, or False otherwise."""
         # TODO: Check if empty
+        return len(self.list) == 0
+
 
     def length(self):
         """Return the number of items in this queue."""
         # TODO: Count number of items
+        return len(self.list)
 
     def enqueue(self, item):
         """Insert the given item at the back of this queue.
         Running time: O(???) – Why? [TODO]"""
         # TODO: Insert given item
+        self.list.append(item)
 
     def front(self):
         """Return the item at the front of this queue without removing it,
         or None if this queue is empty."""
         # TODO: Return front item, if any
+        if len(self.list) == 0:
+            return None
+        else:
+            return self.list.head.data
 
     def dequeue(self):
         """Remove and return the item at the front of this queue,
         or raise ValueError if this queue is empty.
         Running time: O(???) – Why? [TODO]"""
         # TODO: Remove and return front item, if any
+        if len(self.list) == 0:
+            raise ValueError('Empty list')
+        else:
+            item = self.list[0]
+            self.list.remove(self.list[0])
+            return item
 
 
 # Implement LinkedQueue and ArrayQueue above, then change the assignment below
